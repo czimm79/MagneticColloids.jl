e652a00e1a7bdb1b7cd979b4ed2fec201e25022f
Documenter.jl <documenter@juliadocs.github.io>
2022-10-26 19:32:23 +0000

    build based on 34c63a4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MagneticColloids","category":"page"},{"location":"#MagneticColloids","page":"Home","title":"MagneticColloids","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MagneticColloids.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MagneticColloids]","category":"page"}]
+}
